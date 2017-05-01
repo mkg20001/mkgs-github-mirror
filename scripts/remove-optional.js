4c7253b71ede68dca01553b4c3f01213d38f99b2
mkg20001 <mkg20001@gmail.com>
2017-05-01 18:02:16 +0200

    Remove only deleted

--- scripts/remove-optional.js
+++ scripts/remove-optional.js
@@ -1,4 +1,9 @@
 const c = require(process.argv[2])
 const fs = require("fs")
-c.files_optional = {}
+const path = require("path")
+const d = path.dirname(process.argv[2])
+Object.keys(c.files_optional).filter(p => !fs.existsSync(path.join(d, p))).map(p => {
+  console.log("Clear old %s...", p)
+  delete c.files_optional[p]
+})
 fs.writeFileSync(process.argv[2], new Buffer(JSON.stringify(c, null, 2)))
