d6f05416fd18e347daaa61e3da21f5a633ab8cbd
mkg20001 <mkg20001@gmail.com>
2017-05-01 14:39:23 +0200

    Beautify

--- scripts/index-fix.js
+++ scripts/index-fix.js
@@ -1,9 +1,9 @@
-const fs=require("fs")
+const fs = require("fs")
 
-re=/\"([a-z0-9.-]+)\/log\.html\"/gmi
+re = /\"([a-z0-9.-]+)\/log\.html\"/gmi
 
-c=fs.readFileSync(process.argv[2]).toString()
+c = fs.readFileSync(process.argv[2]).toString()
 
-e=c.replace(re,"\"$1.tar.gz/$1/log.html\"")
+e = c.replace(re, "\"$1.tar.gz/$1/log.html\"")
 
-fs.writeFileSync(process.argv[2],new Buffer(e))
+fs.writeFileSync(process.argv[2], new Buffer(e))
--- scripts/w.js
+++ scripts/w.js
@@ -1,32 +1,39 @@
-function w(l,fnc) {
-  var res=[];
+function w(l, fnc) {
+  var res = [];
+
   function run(done) {
-    var i=l.pop();
+    var i = l.pop();
     if (i) {
-      fnc(i,function(e,r) {
+      fnc(i, function (e, r) {
         if (e) return done(e);
         if (r) res.push(r);
         return run(done);
       });
     } else {
-      done(null,res);
+      done(null, res);
     }
   }
   return run;
 }
+
 function lazy() {
-  const list=[];
-  function push(a,f) {
-    list.push({a:a,f:f});
+  const list = [];
+
+  function push(a, f) {
+    list.push({
+      a: a,
+      f: f
+    });
   }
+
   function exec(cb) {
-    new w(list,function(i,done) {
-      i.f(i.a,done);
+    new w(list, function (i, done) {
+      i.f(i.a, done);
     })(cb);
   }
-  this.push=push;
-  this.wait=exec;
-  this.exec=exec;
+  this.push = push;
+  this.wait = exec;
+  this.exec = exec;
 }
-w.lazy=lazy;
-module.exports=w;
+w.lazy = lazy;
+module.exports = w;
