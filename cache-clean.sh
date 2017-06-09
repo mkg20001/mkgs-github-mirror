aa6654012b4f95b4a22ec7fe1c13afaf0e7d0a25
mkg20001 <mkg20001@gmail.com>
2017-06-09 12:09:58 +0200

    Clean more

--- cache-clean.sh
+++ cache-clean.sh
@@ -9,7 +9,7 @@ for u in $orgs $users; do
   rm -rf $u-cache $u-rehost
   for repo in $(dir -w 1 $u/repos); do
     echo " -- Remove cache for repo $repo@$u"
-    rm -rf $u/stagit/$repo $u/stagit.cache/$repo
+    rm -rf $u/stagit/$repo $u/stagit.cache/$repo $u/stagit.cache/$repo.ver
   done
   rm -rf $u/stagit $u/stagit.cache
 done
